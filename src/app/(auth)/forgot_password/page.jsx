0cf266b69dec71753c76c9140f2c0c9f1ff5f2a6
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-14 19:38:36 +0530

    form validation implemeted

--- src/app/(auth)/forgot_password/page.jsx
+++ src/app/(auth)/forgot_password/page.jsx
@@ -2,11 +2,15 @@
 import { useState } from 'react';
 import { apiClient } from '../../../utils/apiClient';
 import { useRouter } from 'next/navigation';
+import { validateEmail } from '../../../utils/validateFormFields';
 
 const ForgotPasswordPage = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
+  const [validationError, setValidationError] = useState('');
 
+  const [error, setError] = useState('');
   const enableDisableBtn = () => {
     if (!email.length) {
       return true;
@@ -16,6 +20,15 @@ const ForgotPasswordPage = () => {
 
   const handleForgotPassword = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    setError('');
+    setValidationError('');
+    if (!validateEmail(email)) {
+      setValidationError('Please enter a valid email address');
+      setIsLoading(false);
+      return;
+    }
+
     try {
       const data = await apiClient.forgotPassword({
         email: email,
@@ -24,15 +37,21 @@ const ForgotPasswordPage = () => {
 
       console.log(data);
       if (data.error) {
-        alert(data.message);
+        setError(data.message);
+        setIsLoading(false);
         return;
       }
-      setEmail('');
 
+      setEmail('');
+      setValidationError('');
+      setError('');
+      setIsLoading(false);
       alert(data.message);
       router.push('/');
     } catch (error) {
       console.log(error);
+      setError('Somthing Went Wrong');
+      setIsLoading(false);
     }
   };
   return (
@@ -41,19 +60,21 @@ const ForgotPasswordPage = () => {
         onSubmit={handleForgotPassword}
         className="max-w-sm w-full rounded border border-gray-400 grid gap-2 p-4"
       >
-        <h1 className="text-center mb-5 font-semibold text-3xl">
-          Forgot Password
-        </h1>
-
+        <h1 className="text-center font-semibold text-3xl">Forgot Password</h1>
+        {error && (
+          <p className="text-sm text-red-500 text-center mt-1">{error}</p>
+        )}
         <input
           type="email"
           required
           placeholder="email"
           value={email}
-          className="border outline-none rounded px-2 py-1.5"
+          className="border outline-none rounded px-2 py-1.5 mt-5"
           onChange={(e) => setEmail(e.target.value)}
         />
-
+        {validationError && (
+          <p className="text-sm text-red-500">{validationError}</p>
+        )}
         <button
           type="submit"
           disabled={enableDisableBtn() ? true : false}
@@ -61,9 +82,16 @@ const ForgotPasswordPage = () => {
             enableDisableBtn()
               ? 'bg-gray-300 text-gray-100 '
               : 'bg-blue-700 text-gray-100 duration-200 hover:bg-blue-800'
-          }px-2 py-1.5 rounded cursor-pointer`}
+          }px-2 py-1.5 rounded cursor-pointer flex justify-center items-center`}
         >
-          Submit
+          {isLoading ? (
+            <svg
+              className="animate-spin h-8 w-8 border-t-transparent border-2 rounded-full"
+              viewBox="0 0 24 24"
+            ></svg>
+          ) : (
+            'Submit'
+          )}
         </button>
       </form>
     </div>
