636a4f4d55c59d50c1a6b0f55bc4443b5ef01f58
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-14 19:40:48 +0530

    Signup created with validation and error handling

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -4,14 +4,20 @@ import { useGlobalContext } from '../../../context/GlobalContext';
 import Link from 'next/link';
 import { useState } from 'react';
 import { apiClient } from '../../../utils/apiClient';
+import { validateEmail, validateName } from '../../../utils/validateFormFields';
 
 const SignupPage = () => {
   const router = useRouter();
   const { setIsLogin } = useGlobalContext();
 
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [fullName, setFullName] = useState('');
-
+  const [error, setError] = useState('');
+  const [validationError, setValidationError] = useState({
+    email: '',
+    fullName: '',
+  });
   const enableDisableBtn = () => {
     if (!email.length || !fullName.length) {
       return true;
@@ -21,6 +27,28 @@ const SignupPage = () => {
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    setError('');
+    setValidationError({ email: '', fullName: '' });
+
+    if (!validateEmail(email)) {
+      setValidationError((prev) => ({
+        ...prev,
+        email: 'Please enter a valid email address',
+      }));
+      setIsLoading(false);
+      return;
+    }
+
+    if (!validateName(fullName)) {
+      setValidationError((prev) => ({
+        ...prev,
+        fullName: 'Please provide a valid name',
+      }));
+      setIsLoading(false);
+      return;
+    }
+
     try {
       const data = await apiClient.signup({
         email: email,
@@ -30,14 +58,18 @@ const SignupPage = () => {
 
       console.log(data);
       if (data.error) {
-        alert(data.message);
+        setIsLoading(false);
+        setError(data.message);
         return;
       }
       setEmail('');
       setFullName('');
+      setIsLoading(false);
       alert(data.message);
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
+      setError('Something went Wrong');
     }
   };
   return (
@@ -46,19 +78,23 @@ const SignupPage = () => {
         onSubmit={handleSignup}
         className="max-w-sm w-full rounded border border-gray-400 grid gap-2 p-4"
       >
-        <h1 className="text-center mb-5 font-semibold text-3xl">
-          Create Account
-        </h1>
+        <h1 className="text-center font-semibold text-3xl">Create Account</h1>
+
+        {error && (
+          <p className="text-sm text-red-500 text-center mt-1">{error}</p>
+        )}
 
         <input
           type="text"
           required
           placeholder="Your Name"
           value={fullName}
-          className="border outline-none rounded px-2 py-1.5 w-full"
+          className="border outline-none rounded px-2 py-1.5 w-full mt-5"
           onChange={(e) => setFullName(e.target.value)}
         />
-
+        {validationError.fullName && (
+          <p className="text-sm text-red-500">{validationError.fullName}</p>
+        )}
         <input
           type="email"
           required
@@ -67,7 +103,9 @@ const SignupPage = () => {
           className="border outline-none rounded px-2 py-1.5"
           onChange={(e) => setEmail(e.target.value)}
         />
-
+        {validationError.email && (
+          <p className="text-sm text-red-500">{validationError.email}</p>
+        )}
         <button
           type="submit"
           disabled={enableDisableBtn() ? true : false}
@@ -75,9 +113,16 @@ const SignupPage = () => {
             enableDisableBtn()
               ? 'bg-gray-300 text-gray-100 '
               : 'bg-blue-700 text-gray-100 duration-200 hover:bg-blue-800'
-          }px-2 py-1.5 rounded cursor-pointer`}
+          }px-2 py-1.5 rounded cursor-pointer flex items-center justify-center`}
         >
-          Sign up
+          {isLoading ? (
+            <svg
+              className="animate-spin h-8 w-8 border-t-transparent border-2 rounded-full"
+              viewBox="0 0 24 24"
+            ></svg>
+          ) : (
+            'Sign up'
+          )}
         </button>
         <p className="text-sm">
           Already have an account?
