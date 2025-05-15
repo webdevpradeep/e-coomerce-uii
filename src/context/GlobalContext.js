e6772111d88f3f1588fee8f87986e2f6c8fd9cde
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-15 18:56:49 +0530

    renew Token and login state initialised

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,15 +1,90 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
-
+import { createContext, useContext, useEffect, useState } from 'react';
+import { getCookie, setCookie } from '../utils/cookies';
+import { apiClient } from '../utils/apiClient';
+import { useRouter } from 'next/navigation';
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-  const [isLogin, setIsLogin] = useState(false);
+  const router = useRouter();
+  const [userProfile, setUserProfile] = useState(null);
+  const [isLogin, setIsLogin] = useState(() => {
+    const access_token = getCookie('access_token');
+
+    if (access_token === null || access_token.length <= 0) {
+      return false;
+    }
+
+    return true;
+  });
+
+  const fetchMyProfile = async () => {
+    try {
+      const data = await apiClient.fetchMyProfile();
+      if (data.error) {
+        alert(data.message);
+        return;
+      }
+      setUserProfile(data);
+      console.log('User Profile', data);
+    } catch (error) {
+      console.log('Error fetching profile', error);
+    }
+  };
+
+  const renewToken = async () => {
+    try {
+      const data = await apiClient.refreshAccessToken();
+
+      const { access_token, access_token_expires_at } = data;
+      const accessTokenExpiresAt = Date.parse(access_token_expires_at);
+
+      const currentMilies = Date.now();
+      setCookie(
+        'access_token',
+        access_token,
+        parseInt(`${(accessTokenExpiresAt - currentMilies) / 1000}`)
+      );
+
+      setIsLogin(true);
+    } catch (error) {
+      cosole.log('Error renewing token', error);
+    }
+  };
+
+  useEffect(() => {
+    const checkLogin = () => {
+      const accessToken = getCookie('access_token');
+      const refreshToken = getCookie('refresh_token');
+      console.log('I am running');
+      if (accessToken || accessToken !== null) {
+        return;
+      }
+
+      if (refreshToken && refreshToken !== null) {
+        renewToken();
+        return;
+      } else {
+        if (isLogin) {
+          window.location.reload();
+        }
+      }
+    };
+
+    const loginInterval = setInterval(checkLogin, 1000);
+    if (isLogin) {
+      fetchMyProfile();
+    }
+
+    return () => clearInterval(loginInterval);
+  }, [isLogin]);
 
   const value = {
     isLogin,
     setIsLogin,
+    userProfile,
+    setUserProfile,
   };
 
   return (
