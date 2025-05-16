914796bb9bbd6eb758bf3829ae2e8fab99691676
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-16 19:10:20 +0530

    Category page under progress

--- src/app/(mainLayout)/shop/page.jsx
+++ src/app/(mainLayout)/shop/page.jsx
@@ -1,15 +1,7 @@
-import React from 'react';
-import Card from '../../../components/Card';
+'use client';
 
 const ShopPage = () => {
-  return (
-    // <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-    //   {[...Array(8)].map((_, index) => (
-    //     <Card key={index} title="Hello I am Title" />
-    //   ))}
-    // </div>
-    <div>Shop Page</div>
-  );
+  return <div>Shop Page</div>;
 };
 
 export default ShopPage;
