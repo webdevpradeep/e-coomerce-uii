3c7deaecbd32a0da58fdc05e51f096b25af95585
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-21 19:25:42 +0530

    protected routes implemented

--- src/app/(mainLayout)/card/page.jsx
+++ src/app/(mainLayout)/card/page.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useGlobalContext } from '../../../context/GlobalContext';
 import { discountedPrice, paiseToRupee } from '../../../utils/calculation';
+import ProtectedRoutes from '../../../components/ProtectedRoutes';
 const MyCartPage = () => {
   const { cart } = useGlobalContext();
 
@@ -31,32 +32,36 @@ const MyCartPage = () => {
   }, [cart]);
 
   return (
-    <div className="grid grid-cols-8 gap-2">
-      <section className="col-span-5 border">
-        {cart.map((product) => (
-          <div className="flex gap-3 items-center">
-            <img src={product.product_thumbnail} className="h-40" />
-            <div>
-              <h3>{product.product_name}</h3>
-              <p>Quantity {product.quantity}</p>
-              <p>
-                &#8377;{' '}
-                {paiseToRupee(discountedPrice(product.price, product.discount))}
-              </p>
+    <ProtectedRoutes>
+      <div className="grid grid-cols-8 gap-2">
+        <section className="col-span-5 border">
+          {cart?.map((product) => (
+            <div className="flex gap-3 items-center">
+              <img src={product.product_thumbnail} className="h-40" />
+              <div>
+                <h3>{product.product_name}</h3>
+                <p>Quantity {product.quantity}</p>
+                <p>
+                  &#8377;{' '}
+                  {paiseToRupee(
+                    discountedPrice(product.price, product.discount)
+                  )}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </section>
-      <div className="col-span-3">
-        <h3>Total : {totalPrice}</h3>
-        <h3>Total Discount : {totalSavedMoney}</h3>
+          ))}
+        </section>
+        <div className="col-span-3">
+          <h3>Total : {totalPrice}</h3>
+          <h3>Total Discount : {totalSavedMoney}</h3>
 
-        <h3>Final Amount : {disPrice}</h3>
-        <button className="bg-yellow-600 text-white px-5 py-1.5 mt-4 rounded-md">
-          Place Order
-        </button>
+          <h3>Final Amount : {disPrice}</h3>
+          <button className="bg-yellow-600 text-white px-5 py-1.5 mt-4 rounded-md">
+            Place Order
+          </button>
+        </div>
       </div>
-    </div>
+    </ProtectedRoutes>
   );
 };
 
