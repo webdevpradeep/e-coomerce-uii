b9363ee3e71c370ded3415fe5b998db6466f8ff5
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-21 19:19:14 +0530

    protected routes implemented

--- src/app/account/layout.jsx
+++ src/app/account/layout.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ProtectedRoutes from '../../components/ProtectedRoutes';
 
 const AccountLayout = ({ children }) => {
-  return <div>AccountLayout</div>;
+  return (
+    <ProtectedRoutes>
+      <div>{children}</div>
+    </ProtectedRoutes>
+  );
 };
 
 export default AccountLayout;
