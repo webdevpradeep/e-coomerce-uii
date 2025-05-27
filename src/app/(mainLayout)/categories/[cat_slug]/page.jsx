d0e1398ac09126ab304a503edb3bed1e7e1c3424
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-05-27 19:40:57 +0530

    Resposive Drawer final

--- src/app/(mainLayout)/categories/[cat_slug]/page.jsx
+++ src/app/(mainLayout)/categories/[cat_slug]/page.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { apiClient } from '../../../../utils/apiClient';
 import ProductCard from '../../../../components/Card';
+import Link from 'next/link';
+import { discountedPrice, paiseToRupee } from '../../../../utils/calculation';
 
 const CategoryProductsPage = () => {
   const { cat_slug } = useParams();
@@ -29,11 +31,51 @@ const CategoryProductsPage = () => {
   }, [cat_slug]);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="p-4">
       {products.length ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
+            <div
+              className="bg-white rounded-xl shadow overflow-hidden"
+              key={product.id}
+            >
+              {/* Product Image */}
+              <div className="relative">
+                <img
+                  src={product.thumbnail}
+                  alt="Product Image"
+                  className="w-full h-48 object-cover"
+                />
+                {/* Discount Tag */}
+                <div className="absolute top-0 right-0 bg-green-500 text-white font-semibold py-1 px-3 rounded-bl-lg">
+                  {product.discount}% OFF
+                </div>
+              </div>
+
+              {/* Product Details */}
+              <div className="p-4 bg-green-50">
+                <Link
+                  href={`/products/${product.slug}`}
+                  className="line-clamp-1 text-sm font-medium text-gray-800"
+                  title={product.product_name}
+                >
+                  {product.product_name}
+                </Link>
+                {/* Price */}
+                <div className="mt-2 flex items-center">
+                  <span className="text-xl font-bold text-gray-900">
+                    {' '}
+                    &#8377;
+                    {paiseToRupee(
+                      discountedPrice(product.price, product.discount)
+                    )}
+                  </span>
+                  <span className="text-sm text-gray-500 line-through ml-2">
+                    &#8377;{paiseToRupee(product.price)}
+                  </span>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       ) : (
